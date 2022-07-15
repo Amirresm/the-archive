036a42ea36006dc4a4d660797f94e86c9a1be4b7
Amirreza Esm <amirreza.77.esmaeili@gmail.com>
2022-07-15 16:25:26 +0430

    config prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
     },
     "extends": [
         "plugin:react/recommended",
-        "airbnb"
+        "airbnb",
+        "plugin:prettier/recommended"
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
@@ -17,8 +18,12 @@ module.exports = {
     },
     "plugins": [
         "react",
-        "@typescript-eslint"
+        "@typescript-eslint",
+        "prettier"
     ],
     "rules": {
-    }
+        "react/react-in-jsx-scope": "off",
+        "react/jsx-filename-extension": [1, { "extensions": [".tsx", ".jsx"] }]
+    },
+    "ignorePatterns": ["node_modules", ".eslintrc.js", "vite.config.ts"]
 }
